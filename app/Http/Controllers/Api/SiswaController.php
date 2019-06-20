e27f640dc3f6d0f9dfc4cdd85308f5e4f302200d
Agung Wahyudi <olipiskandar@gmail.com>
2019-06-20 15:11:03 +0700

    add model siswa

---
+++ app/Http/Controllers/Api/SiswaController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use App\Siswa;
+use Illuminate\Http\Request;
+
+class SiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $siswa = Siswa::all();
+        if (!$siswa) {
+            $response = [
+                'success' => false,
+                'data' => 'Empty',
+                'message' => 'Siswa tidak ditemukan.'
+            ];
+            return response()->json($response, 404);
+        }
+
+        $response = [
+            'success' => true,
+            'data' => $siswa,
+            'message' => 'Berhasil.'
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $siswa = Siswa::find($id);
+        if (!$siswa) {
+            $response = [
+                'success' => false,
+                'data' => 'Empty',
+                'message' => 'Siswa tidak ditemukan.'
+            ];
+            return response()->json($response, 404);
+        }
+
+        $response = [
+            'success' => true,
+            'data' => $siswa,
+            'message' => 'Berhasil.'
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
